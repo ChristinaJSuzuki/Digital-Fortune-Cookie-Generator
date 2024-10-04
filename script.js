a1cb4aceb6bd61b8eff5b9685185c3f9a38fbb30
Christina Suzuki <christina.suzuki0430@gmail.com>
2024-10-04 15:46:18 -0400

    Come up with messages

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ const splitCookie = `
     |         | |         |
      \\_______/   \\_______/
 `;
+
+// Fortune messages
+const fortuneMessages = [
+  "You are about to become $10 poorer.",
+  "When in anger, sing the alphabet.",
+  "The fortune you seek is in another cookie.",
+  "Fail.",
+  "Come back later... I am sleeping.",
+  "It would be best to maintain a low profile for now.",
+  "Error 404: Fortune not found.",
+  "You will be hungry again in one hour.",
+  "Fortune favors the brave.",
+];
