6d85f4a9368f60875455223c19bed04894301a5d
Christina Suzuki <christina.suzuki0430@gmail.com>
2024-10-04 15:40:11 -0400

    Make splitCookie art using ASCII

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const wholeCookie = `
     |             |
      \\___________/
 `;
+
+// Fortune cookie split in half after "crack"
+const splitCookie = `
+      _______     _______
+     /       \\   /       \\
+    |         | |         |
+     \\_______/   \\_______/
+`;
